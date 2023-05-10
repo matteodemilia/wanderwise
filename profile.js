7668b63cc329d3e7225829b5e89a502197366831
matteoDeMilia <matteodemilia@gmail.com>
2023-05-09 17:36:22 -0700

    Mini Itinerary widget

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
-import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+import { getDatabase, ref, child, get, onValue } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -25,6 +25,15 @@ onAuthStateChanged(auth,(user)=>{
     if(user){
         const userId = user.uid;
         const userRef = child(ref(database), 'users/' + userId);
+
+        // grab forum data 
+        const forumRef = ref(database, "forum");
+        const userForumRef = child(forumRef, userId);
+
+        // grab itinerary data 
+        const itineraryRef = ref(database, "itinerary");
+        const userItineraryRef = child(itineraryRef, userId);
+
         //displays user's "username" and their personal info on their profile
         get(userRef).then((snapshot) => {
             if(snapshot.exists()){
@@ -45,11 +54,66 @@ onAuthStateChanged(auth,(user)=>{
         }).catch((error)=>{
             console.error(error);
         });
+
+        // ITINERARY COLUMN
+        onValue(userItineraryRef, (snapshot) => {
+            // Get all itinerary items from the snapshot
+            const itineraryItems = snapshot.val();
+      
+            // Sort itinerary items by start date
+            const sortedItineraryItems = Object.values(itineraryItems).sort((a, b) => {
+              return new Date(a.Startdate) - new Date(b.Startdate);
+            });
+      
+            // Clear the existing itinerary list container
+            const itineraryListContainer = document.querySelector(".itinerary-list-container");
+            itineraryListContainer.innerHTML = "";
+      
+            // Loop through each itinerary item and add it to the itinerary list container
+            for (const itineraryItem of sortedItineraryItems) {
+              // Create an itinerary item element
+              const itineraryItemElement = document.createElement("div");
+              itineraryItemElement.className = "itinerary";
+      
+              // Add the itinerary item details to the element
+              const titleElement = document.createElement("h2");
+              titleElement.id = "itinerary-title";
+              titleElement.style.marginLeft = "10px"
+              titleElement.textContent = itineraryItem.Title;
+              const startdateElement = document.createElement("span");
+              startdateElement.id = "itinerary-list-dates";
+              startdateElement.style.marginLeft = "10px"
+              startdateElement.textContent = formatDate(itineraryItem.Startdate);
+              const enddateElement = document.createElement("span");
+              enddateElement.id = "itinerary-list-dates";
+              enddateElement.textContent = " to " + formatDate(itineraryItem.Enddate); // Add "to" between start and end date
+              itineraryItemElement.appendChild(titleElement);
+              itineraryItemElement.appendChild(startdateElement);
+              itineraryItemElement.appendChild(enddateElement);
+      
+              // Add the itinerary item element to the itinerary list container
+              itineraryListContainer.appendChild(itineraryItemElement);
+              itineraryListContainer.appendChild(document.createElement("hr"));
+            }
+        });
+
+        // FORUM COLUMN
+        onValue(userForumRef, (snapshot) => {
+            // Grab all forum items from the snapshot
+            const forumItems = snapshot.val();
+
+        });
         
     }
     
 });
 
-
-
+// format date function used for the itinerary + Forum posts 
+function formatDate(dateStr) {
+    const date = new Date(dateStr);
+    const month = date.getMonth() + 1;
+    const day = date.getDate()+1;
+    return `${month < 10 ? "0" : ""}${month}-${day < 10 ? "0" : ""}${day}`;
+  }
+  
 
